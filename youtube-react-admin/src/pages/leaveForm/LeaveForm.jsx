ae2af5385ee043805fab508affa87a402084b8cd
anf-tracker <96342394+anf-tracker@users.noreply.github.com>
2022-01-05 22:24:06 +0530

    date validation in leaveform

--- youtube-react-admin/src/pages/leaveForm/LeaveForm.jsx
+++ youtube-react-admin/src/pages/leaveForm/LeaveForm.jsx
@@ -6,13 +6,12 @@ import { useEffect, useState } from "react";
 import { Spinner } from "react-bootstrap";
 import { getSortOrder } from "../../util/utility";
 
-const Cryptr = require('cryptr');
-const cryptr = new Cryptr('myTotalySecretKey');
+const Cryptr = require("cryptr");
+const cryptr = new Cryptr("myTotalySecretKey");
 
 let leaveTypeOptions;
 let resourceOptions;
 export default function LeaveForm(props) {
-
   const history = useHistory();
   const historyLocation = useLocation();
   const loadData = historyLocation.state;
@@ -25,27 +24,40 @@ export default function LeaveForm(props) {
   const [showForm, setShowForm] = useState(false);
 
   const params = useParams();
-  const title = loadData && loadData.name ?'Edit Leave' : 'New Leave';
-  console.log('state', loadData)
+  const title = loadData && loadData.name ? "Edit Leave" : "New Leave";
+  console.log("state", loadData);
 
   useEffect(() => {
-    const sessionData = sessionStorage.getItem('user');
+    const sessionData = sessionStorage.getItem("user");
     if (!sessionData) {
-      history.push('/');
-    }
-    else {
+      history.push("/");
+    } else {
       let data = JSON.parse(sessionData);
-      data.leaveTypes.push({ leaveTypeName: '', leaveTypeValue: '0' });
-      data.leaveTypes.sort(getSortOrder('leaveTypeName'));
-      leaveTypeOptions = data.leaveTypes.map(item => <option key={item.leaveTypeValue} value={item.leaveTypeValue}>{item.leaveTypeName}</option>);
-      data.resources.push({ _id: '0', name: '' });
-      data.resources.sort(getSortOrder('name')); 
-      resourceOptions = data.resources.map(item => <option key={item._id} value={item._id}>{item.name}</option>);
-      if (loadData) { 
+      data.leaveTypes.push({ leaveTypeName: "", leaveTypeValue: "0" });
+      data.leaveTypes.sort(getSortOrder("leaveTypeName"));
+      leaveTypeOptions = data.leaveTypes.map((item) => (
+        <option key={item.leaveTypeValue} value={item.leaveTypeValue}>
+          {item.leaveTypeName}
+        </option>
+      ));
+      data.resources.push({ _id: "0", name: "" });
+      data.resources.sort(getSortOrder("name"));
+      resourceOptions = data.resources.map((item) => (
+        <option key={item._id} value={item._id}>
+          {item.name}
+        </option>
+      ));
+      if (loadData) {
         setLeaveType(loadData.leaveType);
         setResourceId(loadData.resourceId);
-        setStartDate(loadData.startDate && new Date(loadData.startDate).toISOString().substr(0, 10));
-        setEndDate(loadData.endDate && new Date(loadData.endDate).toISOString().substr(0, 10))
+        setStartDate(
+          loadData.startDate &&
+            new Date(loadData.startDate).toISOString().substr(0, 10)
+        );
+        setEndDate(
+          loadData.endDate &&
+            new Date(loadData.endDate).toISOString().substr(0, 10)
+        );
       }
       setShowForm(true);
     }
@@ -66,18 +78,21 @@ export default function LeaveForm(props) {
   };
 
   const onSubmitRequest = (e) => {
-    e.preventDefault(); 
-    const sessionData = sessionStorage.getItem('user');
+    e.preventDefault();
+    const sessionData = sessionStorage.getItem("user");
     let data = JSON.parse(sessionData);
-    let resourceData = data.resources.filter(item => item._id === resourceId)[0];
+    let resourceData = data.resources.filter(
+      (item) => item._id === resourceId
+    )[0];
     const reqData = {
       resourceId: resourceId,
-      name:resourceData['name'],
+      name: resourceData["name"],
       startDate: startDate,
       endDate: endDate,
       leaveType: leaveType,
-    }; 
-    const url =  loadData && loadData._id ?`/updateLeave/${loadData._id}`: `/applyLeave`;
+    };
+    const url =
+      loadData && loadData._id ? `/updateLeave/${loadData._id}` : `/applyLeave`;
     //const url = `/applyLeave`;
     axios
       .post(`${config.apiURL}${url}`, reqData)
@@ -98,53 +113,72 @@ export default function LeaveForm(props) {
     setEndDate("");
     setLeaveType("");
   };
-  let btnDisable = resourceId && resourceId !== '0' && startDate && endDate && leaveType && leaveType !== '0';
-  btnDisable = !btnDisable ? true : false; 
+  let today = new Date().toISOString().split("T")[0];
+
+  let btnDisable =
+    resourceId &&
+    resourceId !== "0" &&
+    startDate &&
+    endDate &&
+    leaveType &&
+    leaveType !== "0";
+  btnDisable = !btnDisable ? true : false;
   if (!showForm) {
-    return <div className="newUser loaderClass">
-      <Spinner animation="grow" />
-    </div >;
+    return (
+      <div className="newUser loaderClass">
+        <Spinner animation="grow" />
+      </div>
+    );
   }
-  return(
-  <div className="newUser">
-    <h1 className="newUserTitle">{title}</h1>
-    <form className="newUserForm" onSubmit={onSubmitRequest}>
-      <div className="newUserItem">
-        <label className="required">Full Name</label>
-           <select onChange={changeResource} value={resourceId}>
-          {resourceOptions}
-        </select>
-      </div>
-      <div className="newUserItem">
-        <label className="required">Start Date</label>
-        <input type="date" value={startDate} onChange={changeStartDate} />
-      </div>
-      <div className="newUserItem">
-        <label className="required">End Date</label>
-        <input type="date" value={endDate} onChange={changeEndDate} />
-      </div>
-      <div className="newUserItem">
-        <label className="required">Leave Type</label>
-        <select onChange={changeSetleaveType} value={leaveType}>
-          {leaveTypeOptions}
-        </select>
+  return (
+    <div className="newUser">
+      <h1 className="newUserTitle">{title}</h1>
+      <form className="newUserForm" onSubmit={onSubmitRequest}>
+        <div className="newUserItem">
+          <label className="required">Full Name</label>
+          <select onChange={changeResource} value={resourceId}>
+            {resourceOptions}
+          </select>
+        </div>
+        <div className="newUserItem">
+          <label className="required">Start Date</label>
+          <input
+            type="date"
+            value={startDate}
+            onChange={changeStartDate}
+            min={today}
+          />
+        </div>
+        <div className="newUserItem">
+          <label className="required">End Date</label>
+          <input
+            type="date"
+            value={endDate}
+            onChange={changeEndDate}
+            min={startDate}
+          />
+        </div>
+        <div className="newUserItem">
+          <label className="required">Leave Type</label>
+          <select onChange={changeSetleaveType} value={leaveType}>
+            {leaveTypeOptions}
+          </select>
         </div>
         <div className="footer">
           <button className="newUserButton" type="submit" disabled={btnDisable}>
-        Submit
-      </button>
-      <button
-        className="cancelButton"
-        type="cancel"
-        onClick={() => {
-          history.push("/leaves");
-        }}
-      >
-        Cancel
-      </button>
+            Submit
+          </button>
+          <button
+            className="cancelButton"
+            type="cancel"
+            onClick={() => {
+              history.push("/leaves");
+            }}
+          >
+            Cancel
+          </button>
         </div>
-      
-    </form>
-  </div>
+      </form>
+    </div>
   );
 }
