121c18e84bd750b94aa3a48e856d1bb6a1404344
anf-tracker <96342394+anf-tracker@users.noreply.github.com>
2022-01-06 20:48:41 +0530

    Merge branch 'main' of https://github.com/Vivekguptaji/leavetracker

--- youtube-react-admin/src/components/locationChart/LocationChart.jsx
+++ youtube-react-admin/src/components/locationChart/LocationChart.jsx
@@ -1,74 +1,90 @@
-import "./locationChart.css";
+// import "./locationChart.css";
 import React, { Component } from "react";
 import Chart from "react-apexcharts";
 
 class LocationChart extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-
-            series: [{
-                name: 'Net Profit',
-                data: [44, 55, 57, 56, 61, 58, 63, 60, 66]
-            }, {
-                name: 'Revenue',
-                data: [76, 85, 101, 98, 87, 105, 91, 114, 94]
-            }, {
-                name: 'Free Cash Flow',
-                data: [35, 41, 36, 26, 45, 48, 52, 53, 41]
-            }],
-            options: {
-                chart: {
-                    type: 'bar',
-                    height: 400,
-                    width: '100%',
-                },
-                plotOptions: {
-                    bar: {
-                        horizontal: false,
-                        columnWidth: '55%',
-                        endingShape: 'rounded'
-                    },
-                },
-                dataLabels: {
-                    enabled: false
-                },
-                stroke: {
-                    show: true,
-                    width: 2,
-                    colors: ['transparent']
-                },
-                xaxis: {
-                    categories: ['Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct'],
-                },
-                yaxis: {
-                    title: {
-                        text: '$ (thousands)'
-                    }
-                },
-                fill: {
-                    opacity: 1
-                },
-                tooltip: {
-                    y: {
-                        formatter: function (val) {
-                            return "$ " + val + " thousands"
-                        }
-                    }
-                }
+    this.state = {
+      series: [
+        {
+          name: "Net Profit",
+          data: [44, 55, 57, 56, 61, 58, 63, 60, 66],
+        },
+        {
+          name: "Revenue",
+          data: [76, 85, 101, 98, 87, 105, 91, 114, 94],
+        },
+        {
+          name: "Free Cash Flow",
+          data: [35, 41, 36, 26, 45, 48, 52, 53, 41],
+        },
+      ],
+      options: {
+        chart: {
+          type: "bar",
+          height: 400,
+          width: "100%",
+        },
+        plotOptions: {
+          bar: {
+            horizontal: false,
+            columnWidth: "55%",
+            endingShape: "rounded",
+          },
+        },
+        dataLabels: {
+          enabled: false,
+        },
+        stroke: {
+          show: true,
+          width: 2,
+          colors: ["transparent"],
+        },
+        xaxis: {
+          categories: [
+            "Feb",
+            "Mar",
+            "Apr",
+            "May",
+            "Jun",
+            "Jul",
+            "Aug",
+            "Sep",
+            "Oct",
+          ],
+        },
+        yaxis: {
+          title: {
+            text: "$ (thousands)",
+          },
+        },
+        fill: {
+          opacity: 1,
+        },
+        tooltip: {
+          y: {
+            formatter: function (val) {
+              return "$ " + val + " thousands";
             },
-
-
-        };
-    }
-    render() {
-        return (
-            <div className="chartL">
-                <Chart options={this.state.options} series={this.state.series} type="bar" height={400} width={500}/>
-            </div>
-        )
-
-    }
+          },
+        },
+      },
+    };
+  }
+  render() {
+    return (
+      <div className="chartL">
+        <Chart
+          options={this.state.options}
+          series={this.state.series}
+          type="bar"
+          height={300}
+          width={500}
+        />
+      </div>
+    );
+  }
 }
 export default LocationChart;
