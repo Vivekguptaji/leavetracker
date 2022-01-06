466205dbe437c3e2f80b15c7926329b82c89b0fb
anf-tracker <96342394+anf-tracker@users.noreply.github.com>
2022-01-06 19:31:17 +0530

    dasgboardchanges

--- youtube-react-admin/src/components/featuredInfo/FeaturedInfo.jsx
+++ youtube-react-admin/src/components/featuredInfo/FeaturedInfo.jsx
@@ -1,25 +1,61 @@
 import "./featuredInfo.css";
 import { ArrowDownward, ArrowUpward } from "@material-ui/icons";
+import {
+  faUsers,
+  faLaptop,
+  faPhone,
+  faHome,
+} from "@fortawesome/free-solid-svg-icons";
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+library.add(faUsers, faLaptop, faPhone, faHome);
 
 export default function FeaturedInfo() {
   return (
     <div className="featured">
       <div className="featuredItem">
-        <span className="featuredTitle">Total Resources</span>
-        <div className="featuredMoneyContainer">
-          <span className="featuredMoney">30</span>
+        <div className="featuredIcons">
+          <FontAwesomeIcon icon={"users"} />
+        </div>
+        <div>
+          <span className="featuredTitle">Total Resources</span>
+          <div className="featuredMoneyContainer">
+            <span className="featuredMoney">30</span>
+          </div>
         </div>
       </div>
       <div className="featuredItem">
-        <span className="featuredTitle">Working</span>
-        <div className="featuredMoneyContainer">
-          <span className="featuredMoney">27</span>
+        <div className="featuredIcons">
+          <FontAwesomeIcon icon={"laptop"} />
+        </div>
+        <div>
+          <span className="featuredTitle">Active Users</span>
+          <div className="featuredMoneyContainer">
+            <span className="featuredMoney">25</span>
+          </div>
         </div>
       </div>
       <div className="featuredItem">
-        <span className="featuredTitle">On Leave</span>
-        <div className="featuredMoneyContainer">
-          <span className="featuredMoney">3</span>
+        <div className="featuredIcons">
+          <FontAwesomeIcon icon={"phone"} />
+        </div>
+        <div>
+          <span className="featuredTitle">Working</span>
+          <div className="featuredMoneyContainer">
+            <span className="featuredMoney">23</span>
+          </div>
+        </div>
+      </div>
+      <div className="featuredItem">
+        <div className="featuredIcons">
+          <FontAwesomeIcon icon={"home"} />
+        </div>
+        <div>
+          <span className="featuredTitle">Leave</span>
+          <div className="featuredMoneyContainer">
+            <span className="featuredMoney">2</span>
+          </div>
         </div>
       </div>
     </div>
